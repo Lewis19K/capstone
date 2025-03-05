337163c67d729c91ad04f6687e537f551e11534c
Lewis19K <luis.corrales@sansano.usm.cL>
2025-03-04 21:21:30 -0300

    feat: Added Semantic HTML + new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <React.Fragment>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </React.Fragment>
   );
 }
 
